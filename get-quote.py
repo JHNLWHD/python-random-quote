d624bf8ed968710034fcd3b21c6b814d5245dd6f
Al-Jhoenil Wahid <aljhoenilw@gmail.com>
2019-08-13 16:29:18 +0800

    Finished the course

--- get-quote.py
+++ get-quote.py
@@ -2,14 +2,20 @@ import random
 def primary():
 #  print ("Keep it logically awesome.")
 
-  f = open("quotes.txt")
-  quotes = f.readlines()
+  f = open("quotes.txt","a+")
+
+  for i in range(2):
+    f.write("Appended line %d\r\n" % (i+1))
+
+
+  #quotes = f.readlines()
   f.close()
 
-  last = len(quotes) - 1
-  rnd = random.randint(0, last)
+  #last = len(quotes) - 1
+  #rnd = random.randint(0, last)
 
-  print (quotes[rnd])
+  #print (quotes[rnd],end = '')
+  #print (quotes[last])
 
 if __name__== "__main__":
   primary()
